b22a3edfb8384749d578f8bb7e171fb90502b9f0
srli19 <srli19@aau.student.dk>
2020-03-17 15:09:49 +0100

    Added JSON file reader and checker

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
+    let json_Read = fs.readFileSync('IDentifier.json');
+    let json_Converted = JSON.parse(json_Read);
     if (request.method == 'POST') {
         response.writeHead(200,{
             'Content-Type': '*',
             'Access-Control-Allow-Origin': '*'
         });
+
+        console.log(json_Converted.ID);
+
         let body = '';
         request.on('data', chunk => {
             body += chunk.toString();
         });   
         request.on('end', () => {
-            if (body == 'Sebastian'){
+            if (json_Converted.ID == '12345'){ //This sould be changed
                 response.end('You have access');
             }else{
                 response.end('You do not have access')
