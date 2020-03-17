25f6ce973ac0aeacf826fd0fc34fe4a595458ad9
srli19 <srli19@aau.student.dk>
2020-03-17 13:09:48 +0100

    Added post validation

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200,{
-        'Content-Type': '*',
-        'Access-Control-Allow-Origin': '*'
-    });
-    let body = '';
-    request.on('data', chunk => {
-        body += chunk.toString();
-    });
-    request.on('end', () => {
-        console.log(body);
-        response.end('You have access');
-    });       
+    if (method == 'POST') {
+        response.writeHead(200,{
+            'Content-Type': '*',
+            'Access-Control-Allow-Origin': '*'
+        });
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            console.log(body);
+            response.end('You have access');
+        });
+    }
+    response.end('You do not have access to do other requests than POST');
 });
 
 server.listen(3000, 'localhost', () => {
