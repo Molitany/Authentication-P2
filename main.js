b68d846916e28d1762b0cfe940eed2f22aee26f8
Sriimoont <57129400+Sriimoont@users.noreply.github.com>
2020-03-18 10:43:22 +0100

    1st change lol
    whack

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
+/** PHYSICAL KEY 
+ * Encryption
+ * Autorun
+ * Private/Public key
+ * Messages er unique private keys er ikke
+ * Kun message er unique og på nøglen. priv. og publ. key er ens for alle nøgler men begge ukendte for brugere. (good)
+ Good fordi der ikke er en database med priv. keys der kan hackes, men lidt mere usikker, da hvis keyen bliver fundet kan den bruges til alt. 
+ Det ene key set der er kan være mere sikker en de seperate keys hvor alting er unique grundet mere plads
+*/
 
+
+const electron = require('electron');
+const crypto = require('crypto');
 const {app, BrowserWindow} = electron;
 
-let mainWindow
+let message = "SecretPassword101.";
+let publicKey;
+let privateKey;
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWindow.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
-})
+crypto.generateKeyPair('rsa',{modulusLength: 1000, publicExponent: 0x10001}, (err, public, private) => {
+    publicKey = public;
+    privateKey = private;
+} )
+
+console.log(publicKey + "+" + privateKey);
