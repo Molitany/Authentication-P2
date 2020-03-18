7c1ce7b1c5d6cbb70cd48f4b7af57b763dd63cf0
Sriimoont <57129400+Sriimoont@users.noreply.github.com>
2020-03-18 13:01:20 +0100

    Random message
    Created loop that creates a random message for login.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /** PHYSICAL KEY 
- * Encryption
- * Autorun
- * Private/Public key
- * Messages er unique private keys er ikke
+ * Encryption (done)
+ * Autorun 
+ * Private/Public key stored
+ * Messages er unique keysets er ikke
  * Kun message er unique og på nøglen. priv. og publ. key er ens for alle nøgler men begge ukendte for brugere. (good)
  Good fordi der ikke er en database med priv. keys der kan hackes, men lidt mere usikker, da hvis keyen bliver fundet kan den bruges til alt. 
  Det ene key set der er kan være mere sikker en de seperate keys hvor alting er unique grundet mere plads
@@ -13,7 +13,6 @@ const electron = require('electron');
 const {app} = electron;
 const crypto = require('crypto');
 
-let message = Buffer.from("Simon's secret password including 1 and A");
 
 const keys = crypto.generateKeyPairSync('rsa', {
     modulusLength: 4096,
@@ -28,13 +27,19 @@ const keys = crypto.generateKeyPairSync('rsa', {
         passphrase: 'top secret'
       }
 });
-
-
 const privateKey = {key: keys.privateKey.toString(), passphrase: 'top secret'};
 const publicKey = keys.publicKey;
-
-console.log(message.toString());
+/*
+To encrypt use
 message = crypto.publicEncrypt(publicKey, message);
-console.log(message);
+To decrypt use
 message = crypto.privateDecrypt(privateKey, message);
-console.log(message.toString());
+*/
+let message = ' ';
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,:;*-_¨^´`+?=)(/&%¤#"!}][{€$£@';
+for(i=0; i<32; i++){
+    message+= characters.charAt(Math.floor(Math.random() * characters.length));
+}
+console.log(message);
+message = Buffer.from(message);
+console.log(message);
