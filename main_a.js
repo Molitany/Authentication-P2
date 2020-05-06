e0a60374e0e69567884a210a62335715d3ca18d1
apouls19 <58029708+apouls19@users.noreply.github.com>
2020-05-06 13:10:47 +0200

    changed stuff
    Fixed submit to database and copy to clipboard

--- main_a.js
+++ main_a.js
@@ -11,18 +11,90 @@ function get_request() {
   });
 }
 
+function post_request(website, password) {
+  fetch('http://localhost:3000', {
+    method: 'POST',
+    body: website + 0x1c + password
+  })
+    .then((response) => {
+      response.text()
+        .then(data => console.log(data));
+    })
+    .catch(err => console.log(err));
+}
+
+// function bigfetch() {
+//   fetch('http://localhost:3001/%27)
+//     .then(res => {
+//       return res.json()
+//         .then(resJSON => {
+//           return resJSON
+//         });
+//     })
+//     .then(resJson => {
+//       console.log(resJson)
+//       fetch('http://localhost:3000/Passwords', {
+//         method: 'POST',
+//         body: JSON.stringify(resJson)
+//       })
+//         .then((response) => {
+//           console.log(response)
+//           response.text()
+//             .then(data => {
+//               console.log(data)
+//               insert_table("tbody", data);
+//             });
+//         })
+//         .catch(err => console.log(err + 'BRERWER'));
+//     })
+//     .catch(err => {
+//       document.getElementById("usbdevice").style = "color:red";
+//       document.getElementById("usbdevice").innerHTML = "Please input USB device and start the server";
+//     })
+// }
+// bigfetch()
+
+// > function check() {
+
+//   let User1 = {
+//     id: 12345,
+//     password: 123
+
+//   }
+//   let input_username = document.getElementById("IDnumber");
+//   let input_password = document.getElementById("password");
+//   try {
+//     if (input_username.value == User1.id && input_password.value == User1.password) {
+//       console.log("2");
+//       return true;
+
+
+//     } else {
+//       console.log("3");
+//       return false;
+//     }
+//   } catch (error) {
+
+//   }
+// }
+
 function passwordTemplate(password) {
   return `
         <tr>
             <td class="url-text">${password.id}</td>
             <td id="psw" title="${password.password}" class="psw-text" onclick="ShowHide(this)">*********</td>
-            <button onclick="copyToClipboard(this)" class="button"><i class="fas fa-copy fa-2x"></i></button>
-        </tr>
+            <td class="button"><button onclick="copyToClipboard(this)"><i class="fas fa-copy fa-2x"></i></button></td>
+            </tr>
       `;
 }
 
-function ShowHide(id){
-  if (id.innerText === "*********"){
+document.getElementById("submit").addEventListener("click", e => {
+  post_request(document.getElementById("Website").value, document.getElementById("Password").value);
+  bigfetch();
+})
+
+function ShowHide(id) {
+  if (id.innerText === "*********") {
     id.innerText = id.title;
   } else if (id.innerText != "*********") {
     id.innerText = "*********";
@@ -31,12 +103,12 @@ function ShowHide(id){
 
 const copyToClipboard = (id) => {
   const toCopy = document.createElement('textarea');
-  
-    toCopy.value = id.parentElement.children[1].children[0].children[0].children[1].title;
-    document.body.appendChild(toCopy);
-    toCopy.select();
-    document.execCommand('copy');
-    document.body.removeChild(toCopy);
+
+  toCopy.value = id.parentElement.parentElement.children[1].title;
+  document.body.appendChild(toCopy);
+  toCopy.select();
+  document.execCommand('copy');
+  document.body.removeChild(toCopy);
 };
 
 get_request().then(password => {
@@ -52,8 +124,9 @@ get_request().then(password => {
 ${password.map(passwordTemplate).join("")}
 </table>
 
-<p><br /><br /><br /><br /></p>
+<p><br /><br /><br /></p>
 <h1 class="footer">This password vault was made by the P2 group at Aalborg University: Thomas Damsgaard, Thor
     Beregaard, Simon Preuss, Sebastian Lindhart, Kenneth Køpke and Andreas Poulsen</h1>
+<p><br /></p>
 `
 });
