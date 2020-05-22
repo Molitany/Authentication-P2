9437ef5afcb3042fd614535c60ec0b5f98d50143
aThir <thor@beregaard.dk>
2020-05-22 15:29:59 +0200

    Modals work now and logout Bangladorian

--- Website/main_a.js
+++ Website/main_a.js
@@ -13,20 +13,30 @@ function get_request() {
 }
 
 function PostPassword(website, password) {
-  if (website == '') return;
-  return new Promise(resolve => {
-    resolve(fetch('https://localhost:3000/PostPassword', {
-      method: 'POST',
-      body: website + '\t' + password
-    }).then(response => {
-      if (!response.ok)
-        throw Error
-    })
-      .catch(err => {
-        console.table(err)
-        //location.href = 'https://localhost:3000';
-      }));
-  });
+  return new Promise((resolve, reject) => {
+    if (website + password.indexOf('\n') == -1 || website == '')
+      reject("Invalid Website/Password")
+    else {
+      resolve(fetch('https://localhost:3000/PostPassword', {
+        method: 'POST',
+        body: website + '\t' + password
+      }).then(response => {
+        if (!response.ok)
+          throw Error
+      })
+        .catch(err => {
+          document.getElementById("ErrorAddText").innerText = err;
+          //location.href = 'https://localhost:3000';
+        }));
+    }
+  })
+    .catch(err => {
+      document.getElementById("ErrorAddText").innerText = err;
+      setTimeout(() => {
+        document.getElementById("ErrorAddText").innerText = '';
+      }, 2000);
+      //location.href = 'https://localhost:3000';
+    });
 }
 
 function bigfetch() {
@@ -70,7 +80,7 @@ function SetUsername({ Username }) {
 
   <div class="modal-body">
   <p>Remember to remove your USB device from the PC as you leave.</p>
-  <button class="logoutBtn">Logout</button>
+  <button class="logoutBtn" onclick="logout()">Logout</button>
   </div>
 
   <div class="modal-footer">
@@ -81,26 +91,89 @@ function SetUsername({ Username }) {
 
 /* Modal for user in top right corner */
 SetUsername({ Username: 'Placeholder' });
-let modal = document.getElementById("simpleModal");
+let loginModal = document.getElementById("simpleModal");
+let editModal = document.getElementById("editSimpleModal");
+let confirmModal = document.getElementById("confirmSimpleModal");
+let confirmButton = document.getElementById("confirmButton");
 let modalBtn = document.getElementById("modalBtn");
-let closeBtn = document.getElementById("closeBtn");
+let closeBtn = document.getElementsByClassName("closeBtn");
+let ChangePasswordButton = document.getElementById("ChangePassword");
+let CancelModalButton = document.getElementsByClassName("CancelButton");
 
 modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', clickOutside);
 
+function openModal(element) {
+  if (element.id == "editPswBtn")
+    editModal.style.display = 'flex';
+  else if (element.id == 'deleteButton')
+    confirmModal.style.display = 'flex'
+  else
+    loginModal.style.display = 'flex';
 
-function openModal() {
-  modal.style.display = 'flex';
+  ChangePasswordButton.addEventListener('click', ChangePassword);
+  ChangePasswordButton.params = element;
+
+  confirmButton.addEventListener('click', DeleteRow);
+  confirmButton.params = element;
+
+  for (let i = 0; i < CancelModalButton.length; i++) {
+    CancelModalButton[i].addEventListener('click', closeModal);
+    CancelModalButton[i].params = element;
+  }
+
+  for (let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].addEventListener('click', closeModal);
+    closeBtn[i].params = element; 
+  }
+
+  window.addEventListener('click', clickOutside);
+  window.addEventListener('keydown', clickOutside);
 }
 
-function closeModal() {
-  modal.style.display = 'none';
+function closeModal(e) {
+  if (e.currentTarget.params.id == "editPswBtn")
+    editModal.style.display = 'none';
+  else if (e.currentTarget.params.id == 'deleteButton')
+    confirmModal.style.display = 'none'
+  else
+    loginModal.style.display = 'none';
+
+  ChangePasswordButton.removeEventListener('click', ChangePassword);
+  confirmButton.removeEventListener('click', DeleteRow);
+
+  for (let i = 0; i < CancelModalButton.length; i++) {
+    CancelModalButton[i].removeEventListener('click', closeModal);
+  }
+
+  for (let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].removeEventListener('click', closeModal);
+  }
 }
 
 function clickOutside(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
+  if (e.target == loginModal) {
+    loginModal.style.display = 'none';
+  } else if (e.target == editModal) {
+    editModal.style.display = 'none';
+  } else if (e.target == confirmModal) {
+    confirmModal.style.display = 'none';
+  } else if (e.key == 'Escape') {
+    editModal.style.display = 'none';
+    loginModal.style.display = 'none';
+    confirmModal.style.display = 'none';
+  } else {
+    return
+  }
+
+  ChangePasswordButton.removeEventListener('click', ChangePassword);
+  confirmButton.removeEventListener('click', DeleteRow);
+
+  for (let i = 0; i < CancelModalButton.length; i++) {
+    CancelModalButton[i].removeEventListener('click', closeModal);
+  }
+
+  for (let i = 0; i < closeBtn.length; i++) {
+    closeBtn[i].removeEventListener('click', closeModal);
   }
 }
 
@@ -110,8 +183,8 @@ function passwordTemplate(password) {
             <td class="url-text">${password.ID}</td>
             <td id="psw" title="${password.password}" class="psw-text" onclick="ShowHide(this)">*********</td>
             <td class="button"><button onclick="copyToClipboard(this)"><i class="fas fa-copy fa-2x"></i></button></td>
-            <td class="buttonDel"><button onclick="DeleteRow(this)"><i class="fas fa-trash-alt fa-2x"></i></button></td>
-            <td class="buttonEdit"><button id="editPswBtn" onclick="ChangePassword(this)"><i class="fas fa-edit fa-2x"></i></button></td>
+            <td class="buttonDel"><button id="deleteButton" onclick="openModal(this)"><i class="fas fa-trash-alt fa-2x"></i></button></td>
+            <td class="buttonEdit"><button id="editPswBtn" onclick="openModal(this)"><i class="fas fa-edit fa-2x"></i></button></td>
         </tr>
       `;
 }
@@ -119,6 +192,7 @@ function passwordTemplate(password) {
 document.getElementById("submit").addEventListener("click", e => {
   PostPassword(document.getElementById("Website").value, document.getElementById("Password").value)
     .then(() => bigfetch())
+    .catch(err => console.log(err));
 })
 
 function ShowHide(id) {
@@ -165,11 +239,12 @@ ${password.map(passwordTemplate).join("")}
 `
 };
 
-function DeleteRow(element, change) {
+function DeleteRow(e) {
+  console.log(e.currentTarget.params.parentElement.parentElement.children[0].innerHTML.replace('\n', '\u000A'))
   let row = {
     UserID: getCookie('user-id'),
-    Password: element.parentElement.parentElement.children[1]['title'],
-    ID: element.parentElement.parentElement.children[0].innerText
+    Password: e.currentTarget.params.parentElement.parentElement.children[1]['title'],
+    ID: e.currentTarget.params.parentElement.parentElement.children[0].innerText
   }
   fetch('https://localhost:3000/', {
     method: 'DELETE',
@@ -183,7 +258,9 @@ function DeleteRow(element, change) {
       location.href = 'https://localhost:3000';
     })
   bigfetch()
+  closeModal(e)
 }
+
 function search() {
   let search_text = document.getElementById("search").value.toLowerCase();
   let table = document.getElementById("table_body");
@@ -198,20 +275,30 @@ function search() {
   }
 }
 
-function ChangePassword(element) {
-  let password
-  if (confirm(`Are you sure you want to change ${element.parentElement.parentElement.children[0].innerText}?`))
-    password = prompt('What should the new password be?');
+function ChangePassword(e) {
+  let password = e.target.parentElement.children[0].value;
   if (password == '') {
     alert('You did not enter a password we assume you changed your mind')
     return
   }
-  DeleteRow(element, true)
-  PostPassword(element.parentElement.parentElement.children[0].innerText, password)
+
+  console.log(e.currentTarget.params.parentElement.parentElement.children[0].innerText)
+
+  DeleteRow(e)
+  PostPassword(e.currentTarget.params.parentElement.parentElement.children[0].innerText, password)
 }
+
+//Stolen from https://javascript.info/cookie#getcookie-name
 function getCookie(name) {
   let matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
   ));
   return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function logout() {
+  document.cookie = 'masterpw=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = 'message=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  document.cookie = 'user-id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  location.href = 'https://localhost:3000';
 }
